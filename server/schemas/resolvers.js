4bdd8469910dcca99d8b15bf6e2a3a1b9e793b03
Nicole Pingar <nicolepingar@yahoo.com>
2021-12-28 10:42:17 -0500

    minimum resolvers done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User, List, Blog } = require("../models");
+const { findByIdAndDelete } = require("../models/Blog");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
@@ -12,7 +13,22 @@ const resolvers = {
         user: async (parent, { _id }) => {
             return User.findById(_id);
         },
-
+        // Query for entire bucket list
+        lists: async () => {
+            return List.find();
+        },
+        // Query for one item in bucket list 
+        list: async (parent, { _id }) => {
+            return List.findById(_id);
+        },
+        // Query for all blog posts
+        blogs: async () => {
+            return Blog.find();
+        },
+        // Query for one blog post
+        blog: async (parent, { _id }) => {
+            return Blog.findById(_id);
+        },
     },
     Mutation: {
         // Mutation to sign up
@@ -36,10 +52,21 @@ const resolvers = {
             console.log(user);
             return { token, user };
         },
-
-
-
-
+        // Mutation to add bucket list item
+        addList: async (parent, { location, urgency }) => {
+            const list = await List.create({ location, urgency });
+            return list;
+        },
+        //Mutation to delete a bucket list item
+        deleteList: async (parent, { _id }) => {
+            const list = List.findByIdAndDelete({ _id });
+            return list;
+        },
+        // Mutation to add blog post
+        addBlog: async (parent, { title, content }) => {
+            const blog = await Blog.create({ title, content });
+            return blog;
+        }
     },
 };
 
