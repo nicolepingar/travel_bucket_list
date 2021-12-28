f4625edd313b5bbbf05ef994309377311d14ce57
Nicole Pingar <nicolepingar@yahoo.com>
2021-12-28 13:15:03 -0500

    date picker working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import './App.css';
+import DateTimer from '../src/components/Timer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <DateTimer />
+    </div >
   );
 }
 
--- client/src/components/Timer/index.js
+++ client/src/components/Timer/index.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Timer = () => {
-
+    const [startDate, setStartDate] = useState(new Date());
     return (
-        <div>Hi</div>
+        <div>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        </div>
     );
 };
 
 export default Timer;
+
+// get trip location & date to store in local storage
+// date picker https://www.npmjs.com/package/react-datepicker
