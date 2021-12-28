b205dbce2198bf9fefdee5eafd2ecba9a65487d1
Nicole Pingar <nicolepingar@yahoo.com>
2021-12-28 10:59:29 -0500

    most typedefs done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,5 @@
 const { gql } = require('apollo-server-express');
-
+//!!! mutations not done!
 const typeDefs = gql`
 type User {
     _id: ID
@@ -7,6 +7,17 @@ type User {
     email: String!
     password: String!
   }
+type Blog {
+    _id: ID
+    title: String!
+    content: String!
+    createdAt: String
+}
+type List {
+    _id: ID
+    location: String!
+    urgency: Int
+}
 type Auth {
     token: ID!
     user: User
@@ -15,6 +26,10 @@ type Query {
     users: [User]
     user(_id: ID!): User
     me: User
+    lists: [List]!
+    list: (_id): List
+    blogs: [Blog]
+    blog: (_id): Blog
    }
 type Mutation {
     addUser(name: String!, email: String!, password: String!): Auth
